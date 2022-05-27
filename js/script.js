d66e95bf098b5accf446a63d61f898bb13666556
hormiguru <alfredo@caltum.com>
2022-05-26 23:30:02 -0500

    Entrega de Incorporar eventos ver2.0

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
-let nombre = "Vanina"
-let genero = "F"
+let nombre //= "Vanina"
+let genero //= "F"
 let arrResumen = []
 let respCategorias = 0
 
@@ -59,49 +59,123 @@ dropCategoria.onchange = () => {
   if (dropCategoria.options[dropCategoria.selectedIndex].value == "Papelería") {
     pregProducto.innerHTML = `Favor de escoger un producto`
     pProducto.innerHTML = `
-    <select id="dropProducto", onChange="test(options[selectedIndex].value)">
+    <select id="dropProducto", onChange="creaCard(options[selectedIndex].innerText,options[selectedIndex].value)">
         <option value="0">Selecciona uno...</option>
-        <option value="Boligrafo"> Boligrafo </option>
-        <option value="Goma"> Goma </option>
-        <option value="Sacapuntas"> Sacapuntas </option>
+        <option value=1> Boligrafo </option>
+        <option value=2> Goma </option>
+        <option value=3> Sacapuntas </option>
       </select>`
   } else if (dropCategoria.options[dropCategoria.selectedIndex].value == "Cafetería") {
     pregProducto.innerHTML = `Favor de escoger un producto`
     pProducto.innerHTML = `
-    <select id="dropProducto", onChange="test(options[selectedIndex].value)">
+    <select id="dropProducto", onChange="creaCard(options[selectedIndex].innerText,options[selectedIndex].value)">
         <option value="0">Selecciona uno...</option>
-        <option value="Nescafe Clasico"> Nescafe Clasico </option>
-        <option value="Cafe Molido"> Cafe Molido </option>
-        <option value="Te de Hierbabuena"> Te de Hierbabuena </option>
+        <option value=4> Nescafe Clasico </option>
+        <option value=5> Cafe Molido </option>
+        <option value=6> Te de Hierbabuena </option>
       </select>`
   } else if (dropCategoria.options[dropCategoria.selectedIndex].value == "Limpieza") {
     pregProducto.innerHTML = `Favor de escoger un producto`
     pProducto.innerHTML = `
-      <select id="dropProducto", onChange="test(options[selectedIndex].value)">
+      <select id="dropProducto", onChange="creaCard(options[selectedIndex].innerText,options[selectedIndex].value)">
         <option value="0">Selecciona uno...</option>
-        <option value="Sanitas"> Sanitas </option>
-        <option value="Papel higienico"> Papel higienico </option>
-        <option value="Jabon liquido"> Jabon liquido </option>
+        <option value=7> Sanitas </option>
+        <option value=8> Papel higienico </option>
+        <option value=9> Jabon liquido </option>
       </select>`
   } else {
     pregProducto.innerHTML = ``
     pProducto.innerHTML = ``
   }
 }
-const contenedor = document.getElementById("container");
+const contenedor = document.getElementById("cardContainer");
 // contenedor.innerHTML = "";
+const cardInfo = document.createElement("div");
 
-function test(info) {
-  console.log(info);
-  const cardInfo = document.createElement("div");
+// los productos
+let productos = [
+  {
+    id: 01,
+    nombre: "Boligrafo",
+    modelo: "BIC-BOL-M250CA",
+    precio: 12,
+    inventario: 40,
+  },
+  {
+    id: 02,
+    nombre: "Goma",
+    modelo: "AZO-GOM-6590",
+    precio: 21.35,
+    inventario: 15,
+  },
+  {
+    id: 03,
+    nombre: "Sacapuntas",
+    modelo: "ACO-SAC-P3888",
+    precio: 785.79,
+    inventario: 3,
+  },
+  {
+    id: 04,
+    nombre: "Nescafe Clasico 60g",
+    modelo: "NES-NCF-60GR",
+    precio: 66.35,
+    inventario: 16,
+  },
+  {
+    id: 05,
+    nombre: "Cafe Molido Intenso Veracruz, 454g",
+    modelo: "CAP-CAF-VER",
+    precio: 143.31,
+    inventario: 22,
+  },
+  {
+    id: 06,
+    nombre: "Te De Hierbabuena",
+    modelo: "MCC-TE-HIERB25",
+    precio: 37.89,
+    inventario: 10,
+  },
+  {
+    id: 07,
+    nombre: "Sanitas",
+    modelo: "LKC-TOAI-92231",
+    precio: 219.20,
+    inventario: 50,
+  },
+  {
+    id: 08,
+    nombre: "Papel higienico",
+    modelo: "TOR-HIGT-700148",
+    precio: 558.65,
+    inventario: 100,
+  },
+  {
+    id: 09,
+    nombre: "Jabon liquido",
+    modelo: "ALF-JABON-5480",
+    precio: 36.79,
+    inventario: 78,
+  },
+]
+
+function creaCard(nombre, id) {
+  console.log("nombre" + nombre);
+  console.log("id" + id)
+  let findProducto = productos.find(elemento => elemento.id == id)
+  console.log(findProducto);
   cardInfo.className = "card"
   cardInfo.style = "width: 18rem;"
   cardInfo.innerHTML = `
-  <img src="./Img/productos/01.jpg" alt="1">
+  <img src="./Img/productos/${findProducto.id}.jpg" alt="${findProducto.nombre}">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">${findProducto.nombre}</h5>
+    <p class="card-text">Modelo: ${findProducto.modelo}</p>
+    <p class="card-text">Precio: ${findProducto.precio}</p>
+    <p class="card-text">Iva: 16% (`+ findProducto.precio * (16 / 100) + `)</p>
+    <p class="card-text">Inventario: ${findProducto.inventario}</p>
+    <p class="card-text"></p>
+    <p class="card-text"></p>
   </div>
 `
   contenedor.appendChild(cardInfo);
@@ -128,28 +202,29 @@ function test(info) {
 //   } while (pass !== "123");
 // }
 
-class Producto {
-  constructor(id, nombre, modelo, precio, iva, inventario, imagen) {
-    this.id = id
-    this.nombre = nombre
-    this.modelo = modelo
-    this.precio = precio
-    this.iva = this.precio * (iva / 100)
-    this.inventario = inventario
-    this.imagen = imagen
+// class Producto {
+//   constructor(id, nombre, modelo, precio, iva, inventario, imagen) {
+//     this.id = id
+//     this.nombre = nombre
+//     this.modelo = modelo
+//     this.precio = precio
+//     this.iva = this.precio * (iva / 100)
+//     this.inventario = inventario
+//     this.imagen = imagen
 
-  }
-}
-// creamos los productos
-let boligrafo = new Producto(01, "Boligrafo", "BIC-BOL-M250CA", 12, 16, 40);
-let goma = new Producto(02, "Goma", "AZO-GOM-6590", 21.35, 16, 15);
-let sacapuntas = new Producto(03, "Sacapuntas", "ACO-SAC-P3888", 785.79, 16, 3);
-let nescafe = new Producto(04, "Nescafe Clasico 60g", "NES-NCF-60GR", 66.35, 16, 15);
-let cafe_molido = new Producto(05, "Cafe Molido Intenso Veracruz, 454g", "CAP-CAF-VER", 143.31, 16, 5);
-let te_de_hierbabuena = new Producto(06, "Te De Hierbabuena", "MCC-TE-HIERB25", 37.89, 16, 14);
-let sanitas = new Producto(07, "Sanitas", "LKC-TOAI-92231", 219.20, 16, 50);
-let papel_higienico = new Producto(08, "Papel higienico", "TOR-HIGT-700148", 558.65, 16, 85);
-let jabon_liquido = new Producto(09, "Jabon liquido", "ALF-JABON-5480", 36.79, 16, 71);
+//   }
+// }
+// // creamos los productos
+// let boligrafo = new Producto(01, "Boligrafo", "BIC-BOL-M250CA", 12, 16, 40);
+// let goma = new Producto(02, "Goma", "AZO-GOM-6590", 21.35, 16, 15);
+// let sacapuntas = new Producto(03, "Sacapuntas", "ACO-SAC-P3888", 785.79, 16, 3);
+// let nescafe = new Producto(04, "Nescafe Clasico 60g", "NES-NCF-60GR", 66.35, 16, 15);
+// let cafe_molido = new Producto(05, "Cafe Molido Intenso Veracruz, 454g", "CAP-CAF-VER", 143.31, 16, 5);
+// let te_de_hierbabuena = new Producto(06, "Te De Hierbabuena", "MCC-TE-HIERB25", 37.89, 16, 14);
+// let sanitas = new Producto(07, "Sanitas", "LKC-TOAI-92231", 219.20, 16, 50);
+// let papel_higienico = new Producto(08, "Papel higienico", "TOR-HIGT-700148", 558.65, 16, 85);
+// let jabon_liquido = new Producto(09, "Jabon liquido", "ALF-JABON-5480", 36.79, 16, 71);
+
 
 
 
